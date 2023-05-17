3035bdb09c18362cc628f434bb9c454d2c1774cd
Raheem Ahmed <SyedRahimKamal@gmail.com>
2023-05-18 03:00:12 +0500

    new class

---
+++ CWH Cl 1.py
@@ -0,0 +1,21 @@
+import pandas
+
+
+
+import sklearn
+
+
+import sys
+print("Python Version")
+print(sys.version)
+print("Version Info")
+print(sys.version_info)
+
+print("Hey i'm a good\"Daddy of all PMLN\"\hjhjgfhgfgd ")
+# You can also add seperator like below code
+print("Hey",1, 2, sep="-")
+#  you can also join first outpuut with the second output
+# For Example:
+
+print("Hey",1, 2, sep="-", end="\n420")
+print("rahim Daddy")
