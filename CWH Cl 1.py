9548d02082a95a905587af9e0869a5a9561f9adf
Raheem Ahmed <SyedRahimKamal@gmail.com>
2023-05-24 02:14:46 +0500

    new class

--- CWH Cl 1.py
+++ CWH Cl 1.py
@@ -1,10 +1,3 @@
-import pandas
-
-
-
-import sklearn
-
-
 import sys
 print("Python Version")
 print(sys.version)
@@ -19,3 +12,22 @@ print("Hey",1, 2, sep="-")
 
 print("Hey",1, 2, sep="-", end="\n420")
 print("rahim Daddy")
+
+# How to Know the type of any Variable? Lets's know this.
+
+a=45
+b=8
+C="Rahim Daddy"
+d=complex(4+8)
+print("the type of a is is:", type(a))
+print("the type of b is is:", type(b))
+print("the type of c is is:", type(c))
+
+print(15+65)
+print(15-65)
+print(15*3)
+print(15/3)
+print(100//13)
+print(5%3)
+
+
